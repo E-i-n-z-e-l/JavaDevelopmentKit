cef7fa7f9da5dcb219ac27c397c117f36903748f
Einzel <einzel0102@mail.ru>
2024-02-13 14:54:24 +0800

    "Семинар №3. Generics."

---
+++ Seminar03/src/main/java/taskOne/Main.java
@@ -0,0 +1,10 @@
+package taskOne;
+
+import java.io.DataInputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        CommonClass<String, DataInputStream, Integer> myClass = new CommonClass<>("abc", new DataInputStream(System.in), 10);
+        System.out.println(myClass.getClassName());
+    }
+}
